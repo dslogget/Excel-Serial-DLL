2810011e04fe9f168defbfd46cec51f4d60e62dc
Daniel Sloggett <48067784+dslogget@users.noreply.github.com>
2019-06-05 08:36:33 +0100

    Changed Serial functions to use SerialC

--- main.c
+++ main.c
@@ -4,65 +4,28 @@
 #include "string.h"
 #include "stdio.h"
 #include "oleauto.h"
+#include "SerialC/SerialC.h"
 
 HANDLE hComm = INVALID_HANDLE_VALUE;
 
-void DLL_EXPORT __stdcall WriteComm(const LPCSTR toWrite){
-//    char buffer [1000] = {0};
-    DWORD bytesWritten = 0;
-
-    //WideCharToMultiByte(CP_UTF8, 0, (LPCWSTR)(toWrite), -1, buffer, 1000, NULL, &unkown);
-    //MessageBoxA(0, toWrite, "DLL Message", MB_OK | MB_ICONINFORMATION);
-
-
-    WriteFile(hComm, toWrite, strlen(toWrite), &bytesWritten, NULL);
+void DLL_EXPORT __stdcall WriteStrToComm(LPCSTR toWrite){
+    SerialC_Write(hComm, (uint8_t*)toWrite, strlen(toWrite));
 }
 
 
 
-BSTR DLL_EXPORT __stdcall ReadComm(){
-    OLECHAR buffer [1000] = {0};
-    DWORD bytesRead = 0;
-
-    ReadFile(hComm, buffer, 999, &bytesRead, NULL);
-
-
-    return SysAllocString(buffer);
+BSTR DLL_EXPORT __stdcall ReadStrFromComm(){
+    OLECHAR* arr;
+    DWORD size;
+    SerialC_Read(hComm, (uint8_t**)&arr, &size);
+    BSTR ret = SysAllocString(arr);
+    free(arr);
+    return ret;
 }
 
 
 void DLL_EXPORT __stdcall Init(const INT32 ComID){
-    if(hComm == INVALID_HANDLE_VALUE){
-        char buf[100];
-        sprintf(buf, "\\\\.\\COM%d", ComID);
-        hComm = CreateFileA(buf, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
-
-        if (hComm == INVALID_HANDLE_VALUE){
-            MessageBoxA(0, "Port Failed to Open", "DLL Message", MB_OK | MB_ICONINFORMATION);
-        }else{
-            //MessageBoxA(0, "Port Opened", "DLL Message", MB_OK | MB_ICONINFORMATION);
-        }
-
-        DCB dcbSerialParams = { 0 }; // Initializing DCB structure
-        dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
-        GetCommState(hComm, &dcbSerialParams);
-        dcbSerialParams.BaudRate = CBR_9600;  // Setting BaudRate = 9600
-        dcbSerialParams.ByteSize = 8;         // Setting ByteSize = 8
-        dcbSerialParams.StopBits = ONESTOPBIT;// Setting StopBits = 1
-        dcbSerialParams.Parity   = NOPARITY;  // Setting Parity = None
-        SetCommState(hComm, &dcbSerialParams);
-
-        COMMTIMEOUTS timeouts = { 0 };
-        timeouts.ReadIntervalTimeout         = 50; // in milliseconds
-        timeouts.ReadTotalTimeoutConstant    = 50; // in milliseconds
-        timeouts.ReadTotalTimeoutMultiplier  = 10; // in milliseconds
-        timeouts.WriteTotalTimeoutConstant   = 50; // in milliseconds
-        timeouts.WriteTotalTimeoutMultiplier = 10; // in milliseconds
-
-        SetCommTimeouts(hComm, &timeouts);
-    }
-
-
+    SerialC_Init(&hComm, (uint16_t)ComID, 9600, 8, 0, 0);
 }
 
 
@@ -79,7 +42,7 @@ DLL_EXPORT BOOL APIENTRY __stdcall DllMain(HINSTANCE hinstDLL, DWORD fdwReason, 
             break;
 
         case DLL_PROCESS_DETACH:
-            CloseHandle(hComm);
+            SerialC_Free(hComm);
             break;
 
         case DLL_THREAD_ATTACH:
